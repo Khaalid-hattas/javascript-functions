06988091fba68bf146e6d3cea8a10d4603d3c693
Khaalid-hattas <161817858+Khaalid-hattas@users.noreply.github.com>
2026-05-29 10:04:05 +0100

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,13 @@
+function greet(name) {
+    return "Hello, " + name + "!";
+}
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+let greetingMessage = greet("Khaalid");
+let sumResult = add(10, 5);
+
+console.log(greetingMessage);
+console.log("The sum is: " + sumResult);
